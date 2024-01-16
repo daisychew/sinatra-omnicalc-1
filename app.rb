abdbf11ca41c0ae2476ce6adc9209d4d99bde2ab
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-01-16 17:35:32 +0000

    finished /square/new

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @input_num = params.fetch("users_number")
+  @result_num = params.fetch("users_number").to_f ** 2
+
+  erb(:squared)
+end
